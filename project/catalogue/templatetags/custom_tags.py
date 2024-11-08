f9aabf249247fb91dad71c93c091f5d2adb05297
Constantin <Constantin_CK@outlook.com>
2024-11-08 23:43:19 +0300

    Style some template

--- project/catalogue/templatetags/custom_tags.py
+++ project/catalogue/templatetags/custom_tags.py
@@ -13,9 +13,19 @@ def get_active_class(context, url: str, *args) -> str:
     return ""
 
 
+# Пользовательский тег для шаблона для изменения класса тега
+# (Проверка ссылки на important=True)
 @register.simple_tag(takes_context=False)
 def is_important_link(data) -> str:
-    print('!!!!!!', data)
     if data:
         return "important-link"
+    return ""
+
+
+# Пользовательский тег для шаблона для добавления в шаблон смайлика
+# (Проверка ссылки на important=True)
+@register.simple_tag(takes_context=False)
+def smile_for_important_link(data) -> str:
+    if data:
+        return "😍"
     return ""
